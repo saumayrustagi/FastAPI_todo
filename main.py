6763fcfeb3878fc3ddf913465423f4db3126c6f6
saumayrustagi <saumay03pro@gmail.com>
2025-04-23 10:49:54 +0530

    use async due to performance improvement

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Todo(BaseModel):
     status: bool
 
 @app.get("/getAll", response_model=list[Todo])
-def get_all_todos():
+async def get_all_todos():
     cursor = collection.find({})
     items: list[Todo] = []
     for doc in cursor:
@@ -24,29 +24,29 @@ def get_all_todos():
 
 
 @app.get("/get/{todo_id}", response_model=Todo)
-def get_todo(todo_id: str):
+async def get_todo(todo_id: str):
     todo = collection.find_one({"_id": ObjectId(todo_id)})
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return {"name": todo["name"], "status": todo["status"]}
 
 @app.post("/create", response_model=Todo)
-def create_todo(todo: Todo):
+async def create_todo(todo: Todo):
     result = collection.insert_one(todo.model_dump())
     return todo
 
 @app.post("/createMany", response_model=list[Todo])
-def create_many_todos(todos: list[Todo]):
+async def create_many_todos(todos: list[Todo]):
     result = collection.insert_many([todo.model_dump() for todo in todos])
     return todos
 
 @app.put("/update/{todo_id}", response_model=Todo)
-def update_todo(todo_id: str, todo: Todo):
+async def update_todo(todo_id: str, todo: Todo):
     result = collection.update_one({"_id": ObjectId(todo_id)}, {"$set": todo.model_dump()})
     return todo
 
 @app.put("/updateMany", response_model=dict)
-def update_many( filter: dict = Body(..., embed=True), update: dict = Body(..., embed=True)):
+async def update_many( filter: dict = Body(..., embed=True), update: dict = Body(..., embed=True)):
     result = collection.update_many(filter, {"$set": update})
     return {
         "matched_count": result.matched_count,
@@ -54,14 +54,14 @@ def update_many( filter: dict = Body(..., embed=True), update: dict = Body(..., 
     }
 
 @app.delete("/delete/{todo_id}", response_model=dict)
-def delete_todo(todo_id: str):
+async def delete_todo(todo_id: str):
     result = collection.delete_one({"_id": ObjectId(todo_id)})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Todo not found")
     return {"deleted": todo_id}
 
 @app.delete("/deleteMany", response_model=dict)
-def delete_many_todos(ids: list[str]):
+async def delete_many_todos(ids: list[str]):
     object_ids = [ObjectId(todo_id) for todo_id in ids]
     result = collection.delete_many({"_id": {"$in": object_ids}})
     return {"deleted_count": result.deleted_count}
